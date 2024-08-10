cab96584f4cf6bf996b516f4a8dad5f54b2640a4
Octavia Togami <octavia.togami@gmail.com>
2024-08-10 15:10:45 -0700

    Inject ReadTheDocs stuff into docs

--- source/conf.py
+++ source/conf.py
@@ -37,6 +37,14 @@ extensions = [
     'enginehub.sphinx_youtube'
 ]
 
+# Inject ReadTheDocs stuff
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
